c18e7327dd6a3452ab4d12bf8b75cf50a244f594
Ollie Norris <norrisollie@icloud.com>
2024-06-17 22:12:30 +0100

    Fixed issue where page title was not working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
-import Bio from "@/components/Bio"
-import Contact from "@/components/Contact"
-import Hero from "@/components/Hero"
-import Articles from "@/components/Articles"
+import Bio from "@/components/Bio";
+import Contact from "@/components/Contact";
+import Hero from "@/components/Hero";
+import Articles from "@/components/Articles";
+import { Metadata } from "next/types";
+
+export const metadata: Metadata = {
+  title:
+    "Johnathon Spectre | Helping companies build better, scalable software",
+};
 
 const Home = () => {
-	return (
-		<div>
-			<Hero />
-			<Articles />
-			<Bio />
-			<Contact />
-		</div>
-	)
-}
+  return (
+    <div>
+      <Hero />
+      <Articles />
+      <Bio />
+      <Contact />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
